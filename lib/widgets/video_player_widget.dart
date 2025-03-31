7d28db867c5d5fa33ee66fd8dfa1b04211611f06
Chethan616 <chethankrishna2022@gmail.com>
2025-03-31 19:54:13 +0530

    some minor changes

--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -1,5 +1,7 @@
-import 'package:cached_video_player/cached_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
   const VideoPlayerWidget({
@@ -7,71 +9,145 @@ class VideoPlayerWidget extends StatefulWidget {
     required this.videoUrl,
     required this.color,
     required this.viewOnly,
+    this.autoPlay = false,
+    this.looping = false,
   });
 
   final String videoUrl;
   final Color color;
   final bool viewOnly;
+  final bool autoPlay;
+  final bool looping;
 
   @override
   State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
 }
 
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late CachedVideoPlayerController videoPlayerController;
-  bool isPlaying = false;
-  bool isLoading = true;
+  late VideoPlayerController _videoController;
+  ChewieController? _chewieController;
+  bool _isLoading = true;
+  bool _showReplay = false;
+  bool _isReplaying = false;
 
   @override
   void initState() {
-    videoPlayerController = CachedVideoPlayerController.network(
-      widget.videoUrl,
-    )
-      ..addListener(() {})
-      ..initialize().then((_) {
-        videoPlayerController.setVolume(1);
-        setState(() {
-          isLoading = false;
-        });
-      });
     super.initState();
+    _initializePlayer();
+  }
+
+  Future<void> _initializePlayer() async {
+    try {
+      final file = await DefaultCacheManager().getSingleFile(widget.videoUrl);
+      _videoController = VideoPlayerController.file(file)
+        ..setLooping(widget.looping)
+        ..addListener(_checkVideoStatus);
+
+      await _videoController.initialize();
+
+      _chewieController = ChewieController(
+        videoPlayerController: _videoController,
+        autoPlay: widget.autoPlay,
+        looping: widget.looping,
+        allowMuting: true,
+        allowPlaybackSpeedChanging: false,
+        showControls: !widget.viewOnly,
+        materialProgressColors: ChewieProgressColors(
+          playedColor: widget.color,
+          handleColor: widget.color,
+          bufferedColor: Colors.grey.shade300,
+        ),
+        placeholder: Container(color: Colors.black),
+        autoInitialize: true,
+      );
+
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+      debugPrint('Video player error: $e');
+    }
+  }
+
+  void _checkVideoStatus() {
+    if (_videoController.value.isInitialized &&
+        !_videoController.value.isPlaying &&
+        _videoController.value.position == _videoController.value.duration) {
+      if (mounted) {
+        setState(() => _showReplay = true);
+      }
+    } else {
+      if (mounted && _showReplay) {
+        setState(() => _showReplay = false);
+      }
+    }
+  }
+
+  Future<void> _replayVideo() async {
+    setState(() {
+      _isReplaying = true;
+      _showReplay = false;
+    });
+
+    await _videoController.seekTo(Duration.zero);
+    await _videoController.play();
+
+    if (mounted) {
+      setState(() => _isReplaying = false);
+    }
   }
 
   @override
   void dispose() {
-    videoPlayerController.dispose();
+    _videoController.removeListener(_checkVideoStatus);
+    _videoController.dispose();
+    _chewieController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 16 / 9,
+      aspectRatio: 16 / 16,
       child: Stack(
         children: [
-          isLoading
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : CachedVideoPlayer(videoPlayerController),
-          Center(
-            child: IconButton(
-              icon: Icon(
-                isPlaying ? Icons.pause : Icons.play_arrow,
-                color: widget.color,
+          if (_isLoading && !_isReplaying)
+            Container(
+              color: Colors.black,
+              child: const Center(child: CircularProgressIndicator()),
+            )
+          else if (_chewieController == null)
+            Container(
+              color: Colors.black,
+              child: const Center(
+                child: Icon(Icons.error_outline, color: Colors.red, size: 40),
               ),
-              onPressed: widget.viewOnly
-                  ? null
-                  : () {
-                      setState(() {
-                        isPlaying = !isPlaying;
-                        isPlaying
-                            ? videoPlayerController.play()
-                            : videoPlayerController.pause();
-                      });
-                    },
+            )
+          else
+            Chewie(controller: _chewieController!),
+
+          // Replay button overlay
+          if (_showReplay && !_isReplaying)
+            Center(
+              child: GestureDetector(
+                onTap: _replayVideo,
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.7),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.replay,
+                    color: widget.color,
+                    size: 40,
+                  ),
+                ),
+              ),
             ),
-          ),
         ],
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,9 +16,12 @@ import firebase_messaging
 import firebase_storage
 import flutter_app_badger
 import flutter_local_notifications
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
+import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -32,7 +35,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
